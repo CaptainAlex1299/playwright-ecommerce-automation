0000917b0bf4fd2b977d99f04562dea19d878c36
CaptainAlex1299 <alexanderdietstein@gmail.com>
2026-05-12 12:56:02 +0200

    added another test to login.spec.js with invalid credentials to login

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -4,9 +4,19 @@ test('should login user successfully', async ({ page }) => {
     await page.goto('https://automationexercise.com/');
     await expect(page.getByText('Signup / Login')).toBeVisible();
     await page.getByRole('link', { name: 'Signup / Login' }).click();
-    await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill('alexEmail@gmail.com');
-    await page.getByRole('textbox', { name: 'Password' }).fill('myPassword');
+    await page.locator('[data-qa="login-email"]').fill('alexEmail@gmail.com');
+    await page.locator('[data-qa="login-password"]').fill('myPassword');
     await page.getByRole('button', { name: 'Login' }).click();
     await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
     await page.getByRole('link', { name: 'Logout' }).click();
+});
+
+test('should not login with invalid credentials', async ({ page }) => {
+    await page.goto('https://automationexercise.com/');
+    await expect(page.getByText('Signup / Login')).toBeVisible();
+    await page.getByRole('link', { name: 'Signup / Login' }).click();
+    await page.locator('[data-qa="login-email"]').fill('wrongEmail@gmail.com');
+    await page.locator('[data-qa="login-password"]').fill('wrongPassword');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page.getByText('Your email or password is incorrect')).toBeVisible();
 });
