153d07dedfbf14f1d265f1a76ca648fb69d1bd4c
CaptainAlex1299 <alexanderdietstein@gmail.com>
2026-05-13 13:29:02 +0200

    test: add logout test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,18 +1,25 @@
 import { test, expect, Page } from '@playwright/test';
 
-async function login(page: Page){
+async function login(page: Page) {
     await page.goto('https://automationexercise.com/');
     await expect(page.getByText('Signup / Login')).toBeVisible();
     await page.getByRole('link', { name: 'Signup / Login' }).click();
     await page.locator('[data-qa="login-email"]').fill('alexEmail@gmail.com');
     await page.locator('[data-qa="login-password"]').fill('myPassword');
     await page.getByRole('button', { name: 'Login' }).click();
-}
+};
 
 test('should login user successfully', async ({ page }) => {
     await login(page);
     await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
     await page.getByRole('link', { name: 'Logout' }).click();
+});
+
+test('logout successfully', async ({ page }) => {
+    await login(page);
+    await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
+    await page.getByRole('link', { name: 'Logout' }).click();
+    await expect(page.getByRole('link', { name: 'Logout' })).toHaveCount(0);
 });
 
 test('should not login with invalid credentials', async ({ page }) => {
