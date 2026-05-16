142426700ba6a677c9642ac89b24a6ee08085992
CaptainAlex1299 <alexanderdietstein@gmail.com>
2026-05-16 16:44:20 +0200

    test: search a product

--- tests/verify.spec.ts
+++ tests/verify.spec.ts
@@ -2,7 +2,7 @@ import { test, expect, Page } from '@playwright/test';
 import { blockPopups } from './utils/popopBlocker';
 
 test.beforeEach(({ page }) => {
-     blockPopups(page);
+    blockPopups(page);
 });
 
 test('verify test cases', async ({ page }) => {
@@ -17,4 +17,14 @@ test('verify products on home page', async ({ page }) => {
     await page.goto("https://automationexercise.com/");
     await expect(page.locator('.product-overlay')).toHaveCount(34);
 });
+
+test('search a product', async ({ page }) => {
+    await page.goto("https://automationexercise.com/");
+    await page.getByRole('link', { name: 'Products' }).click();
+    await expect(page.locator('#search_product')).toBeVisible();
+    await page.locator('#search_product').fill('Summer White Top');
+    await page.locator('#submit_search').click();
+    await page.locator('#search_product').clear();
+    await expect(page.locator('img[src="/get_product_picture/6"]')).toBeVisible();
+})
 
